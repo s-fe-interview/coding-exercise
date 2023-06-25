38cbbfb89ba6d4ed35ab5e10ec1a6dba22d96fed
raja-ui-dev <gajjela.raja1@gmail.com>
2023-06-25 16:37:38 +0200

    Adding code for Satcom coding exercise

--- satcom-test/src/app/components/list/list.component.ts
+++ satcom-test/src/app/components/list/list.component.ts
@@ -1,6 +1,9 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {Subscription} from "rxjs";
+import { Customer } from 'src/app/models/customer';
+import { Product } from 'src/app/models/product';
+import { MockDataService } from 'src/app/services/mock-data.service';
 
 @Component({
   selector: 'app-list',
@@ -10,13 +13,17 @@ import {Subscription} from "rxjs";
 export class ListComponent implements OnInit, OnDestroy {
   filterForm: FormControl;
   subscriptions = new Subscription();
+  elements: (Product | Customer)[];
+  filteredListElements: (Product | Customer)[];
+  @Output() elementSelected: EventEmitter<Product | Customer> = new EventEmitter<Product | Customer>();
 
-  constructor() {
+  constructor(private mockDataService: MockDataService) {
 
   }
 
   ngOnInit(): void {
-    this.initForm()
+    this.initForm();
+    this.getData()
   }
 
   ngOnDestroy() {
@@ -32,7 +39,44 @@ export class ListComponent implements OnInit, OnDestroy {
     )
   }
 
-  onFilterChange(inputSearched: string) {
-
+  getData(): void {
+    this.subscriptions.add(
+      this.mockDataService.getData().subscribe((data) => {
+        this.elements = data;
+        this.filteredListElements = data;
+      })
+    );
   }
+
+  onFilterChange(inputSearched: string) {
+    const filteredElements = this.elements.filter((element) => {
+      if ('birthDate' in element) {
+        return element.name.toLowerCase().includes(inputSearched.toLowerCase());
+      } else if ('productNumber' in element && 'price' in element && 'premium' in element) {
+        const product = element as Product;
+        const currency = product.premium ? 'USD' : 'EUR';
+        const searchValue = inputSearched.toLowerCase();
+        const nameMatch = product.name.toLowerCase().includes(searchValue);
+        const priceMatch = (this.convertCurrency(product.price, currency)).toString().includes(searchValue);
+        return !nameMatch && !priceMatch ? product.premium : true;
+      }
+      return false;
+    });
+  
+    this.filteredListElements = filteredElements;
+  }
+
+  convertCurrency(price: number, currency: string): string {
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: currency
+    });
+  
+    return formatter.format(price);
+  }
+  
+  selectElement(element: Product | Customer): void {
+    this.elementSelected.emit(element);
+  }
+
 }
